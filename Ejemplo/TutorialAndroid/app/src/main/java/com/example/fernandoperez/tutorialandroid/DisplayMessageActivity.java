c65a589537bb20e1359dc115566ccc18f7a1e2d4
Fernando Pérez <fernandoperez@Fernandos-iMac.local>
2015-04-08 20:19:28 -0430

    Todos los archivos del curso de android

---
+++ Ejemplo/TutorialAndroid/app/src/main/java/com/example/fernandoperez/tutorialandroid/DisplayMessageActivity.java
@@ -0,0 +1,43 @@
+package com.example.fernandoperez.tutorialandroid;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class DisplayMessageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Recibimos el mensaje y lo pasamos a un String.
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        //Ahora lo vamos a mostrar en pantalla.
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        //Ahora ponemos que haga eso como principal.
+        setContentView(textView);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
