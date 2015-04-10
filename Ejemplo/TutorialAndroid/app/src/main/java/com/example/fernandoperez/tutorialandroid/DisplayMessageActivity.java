2271534e4a2b45eb576bece66f04117175dba048
Fernando Pérez <fernandoperez@Fernandos-iMac.local>
2015-04-09 23:21:28 -0430

    Añadido soporte de español

--- Ejemplo/TutorialAndroid/app/src/main/java/com/example/fernandoperez/tutorialandroid/DisplayMessageActivity.java
+++ Ejemplo/TutorialAndroid/app/src/main/java/com/example/fernandoperez/tutorialandroid/DisplayMessageActivity.java
@@ -1,8 +1,8 @@
 package com.example.fernandoperez.tutorialandroid;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
--- Ejemplo/TutorialAndroid/app/src/main/java/com/example/fernandoperez/tutorialandroid/MyActivity.java
+++ Ejemplo/TutorialAndroid/app/src/main/java/com/example/fernandoperez/tutorialandroid/MyActivity.java
@@ -1,8 +1,8 @@
 package com.example.fernandoperez.tutorialandroid;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
